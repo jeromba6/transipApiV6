8870c96fcea3ff17ecbb40fbbae25082d58f554c
Jeroen van Gemert <jeroen.vangemert@kpn.com>
2020-03-15 21:31:49 +0100

    Rename, make use of _ instead of -

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="transip-api-v6", # Replace with your own username
+    name="transip_api_v6", # Replace with your own username
     version="0.0.1",
     author="Jeroen van Gemert",
     author_email="pypi@jeroen.van.gemert.net",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.x',
 )
